902d4914c74a76a6a1aab347ddb76341f24dc0b4
wodahS21 <yaelelyolo@gmail.com>
2023-11-01 09:25:00 -0600

    Modificacion app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 
 import { AppComponent } from './app.component';
+import { DadoComponent } from './dado/dado.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadoComponent
   ],
   imports: [
     BrowserModule
